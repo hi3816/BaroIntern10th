6af127e4586164753232992d12e81cf21094d30f
hi3816 <wlgp2534@naver.com>
2025-03-13 22:49:12 +0900

    [Feat] Bullet 추가

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damage; //데미지
+    public int per; //관통
+
+    public void Init(float damage, int per)
+    {
+        this.damage = damage;
+        this.per = per;
+    }
+}
